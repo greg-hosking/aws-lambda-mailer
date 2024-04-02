e7209eff718590b3377f49a3bec6224bac70fced
Greg Hosking <hoskinggregory@gmail.com>
2024-04-02 00:36:31 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import { createTransport } from "nodemailer";
+
+const transporter = createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS,
+    },
+});
+
+const sendEmail = async () => {
+    try {
+        const mailOptions = {
+            from: process.env.GMAIL_USER,
+            to: "email@example.com",
+            subject: "Example Subject",
+            text: "Example email body.",
+        };
+        await transporter.sendMail(mailOptions);
+    } catch (error) {
+        throw error;
+    }
+};
+
+export async function handler(event) {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Hello from Lambda!", event: event }),
+    };
+    return response;
+}
