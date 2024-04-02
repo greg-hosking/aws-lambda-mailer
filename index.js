c1642dfae949d8ff687e78719f82fad0bd569f41
Greg Hosking <hoskinggregory@gmail.com>
2024-04-02 00:48:26 -0400

    Revert nodemailer import change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const nodemailer = require("nodemailer");
+import { createTransport } from "nodemailer";
 
-const transporter = nodemailer.createTransport({
+const transporter = createTransport({
     service: "gmail",
     auth: {
         user: process.env.GMAIL_USER,
